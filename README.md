78978b93ae4537fd57a762d1ee9688994935d509
Adam Lieth P <adamleithp@gmail.com>
2018-01-30 21:59:48 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# BaseTwo Website
+
+### Installation
+
+* ```git clone https://github.com/basetwo-project/basetwo-project.github.io.git```
+* ```npm install```
+* ```npm run dev``` to compile code and run local server
+
+Now go to http://localhost:3000
+
+### Deploy to Github Pages
+```npm run deploy```
---
+++ gruntfile.js
@@ -0,0 +1,171 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    clean: {
+      html: {
+        src: '_site/*.html'
+      },
+      js: {
+        src: '_site/js/'
+      },
+      css: {
+        src: '_site/css/'
+      },
+    },
+
+
+    copy: {
+      dist: {
+        expand: true,
+        cwd: 'src/',
+        src: [
+          'images/**/*',
+          'CNAME'
+        ],
+        dest: '_site/',
+      },
+    },
+
+
+    browserSync: {
+      bsFiles: {
+        src: '_site/**',
+      },
+      options: {
+        watchTask: true,
+        server: './_site',
+        startPath: `/`,
+        open: false,
+        notify: false
+      },
+    },
+
+
+    nunjucks: {
+      options: {
+        data: grunt.file.readJSON('data.json')
+      },
+      render: {
+        files: [
+          {
+            expand: true,
+            cwd: "src/html/pages/",
+            src: "**/*.njk",
+            dest: "_site/",
+            ext: ".html",
+          }
+        ]
+      }
+    },
+
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: "src/css/",
+          src: "*.scss",
+          dest: "_site/css",
+          ext: '.css'
+        }]
+      }
+    },
+
+
+    browserify: {
+      options: {
+        transform: [['babelify', { presets: "es2015" }]]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/js/',
+          src: [
+            '**/*.js',
+          ],
+          dest: '_site/js/',
+        }]
+      }
+    },
+
+
+    watch: {
+      html: {
+        files: [
+          'src/html/**/*.njk',
+        ],
+        tasks: [
+          'clean:html',
+          'nunjucks',
+        ],
+      },
+
+      css: {
+        files: [
+          'src/css/*.scss',
+          'src/css/*.css',
+        ],
+        tasks: [
+          'clean:css',
+          'sass',
+        ],
+      },
+
+      js: {
+        files: [
+          'src/js/**/*.js'
+        ],
+        tasks: [
+          'clean:js',
+          'browserify:dist'
+        ]
+      },
+
+      images: {
+        files: [
+          'src/images/**'
+        ],
+        tasks: [
+          'copy:dist'
+        ]
+      }
+    },
+
+
+    'gh-pages': {
+      options: {
+        base: '_site'
+      },
+      src: ['**']
+    }
+  });
+
+  require('load-grunt-tasks')(grunt, {
+    scope: 'devDependencies'
+  });
+
+  grunt.registerTask('default', [
+    'browserSync',
+    'clean:html',
+    'clean:js',
+    'clean:css',
+    'nunjucks',
+    'sass:dist',
+    'browserify:dist',
+    'copy:dist',
+    'watch',
+  ]);
+
+  grunt.registerTask('deploy', [
+    'clean:html',
+    'clean:js',
+    'clean:css',
+    'nunjucks',
+    'sass:dist',
+    'browserify:dist',
+    'copy:dist',
+    'gh-pages'
+  ]);
+}
